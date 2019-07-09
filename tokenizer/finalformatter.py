f23f22685b407b405fa82a0c472d42974b1b02cf
na3la <tm.sc@protonmail.com>
2019-07-09 03:39:25 -0700

    updated formatter

--- tokenizer/finalformatter.py
+++ tokenizer/finalformatter.py
@@ -17,17 +17,18 @@ class fmt:
 
     def _fixfilepath(self):
 
+        if not os.path.isdir(self.pathtofixed):
+            print('Creating directory {}', self.pathtofixed)
+            os.mkdir(self.pathtofixed)
         if self.pathtofixed[-1] != '/':
             self.pathtofixed = ''.join((self.pathtofixed, '/'))
         if self.pathtoold[-1] != '/':
             self.pathtoold = ''.join((self.pathtofixed, '/'))
         try:
-            os.path.exists(self.pathtofixed)
             os.path.exists(self.pathtoold)
         except FileNotFoundError:
-            print('Error: check path for fixed and path to original files. ')
-            print('fixed: {}', self.pathtofixed)
-            print('orig: {}', self.pathtoold)
+            print('Error: check path to unformatted files. ')
+            print('Provided path: {}', self.pathtoold)
 
     def _read_space(self):
         self._fixfilepath()
@@ -37,82 +38,77 @@ class fmt:
             if os.path.isdir(filename) or filename[-5:] != ".java":
                 continue
             f = open(self.pathtoold + filename, 'r')
+            self.__TEST__(f, filename)
+            continue
+            f = open(self.pathtoold + filename, 'r')
             _to_analyze = f.read()
             f.close()
             if not len(_to_analyze):
                 continue
-            self._cleaning(_to_analyze, filename)
+            # try yielding _to_analyze and filename, and feed to cleaning
+            fixed_text = self._cleaning(_to_analyze, filename)
+            if fixed_text:
+                self._write_fixed(fixed_text, filename)
 
-    def _cleaning(self, f, name):
+    def __TEST__(self, f, filename):
+        check = True
+        begin_write = False
+        test = re.compile(r'//\s?.*')
+        for line in f:
+            line = line.strip()
+            if test.search(line):
+                line = test.sub('', line)
+            if re.search(r'/\*\*', line) and re.search(r'\*/', line):
+                line = re.sub(r'/\*\*.*?\*/', '', line)
+            elif re.search(r'/\*\*', line):
+                line = re.sub(r'/\*\*.*?', '', line)
+                check = False
+            elif not check and not re.search(r'\*/', line):
+                continue
+            elif re.search(r'\*/', line):
+                check = True
+                continue
+                re.sub(r'.*?\*/', '', line)
+            if line.find('eval(') != -1:
+                begin_write = True
+            elif begin_write and (line.find('@Override') != -1
+                                  or line.find('public') != -1):
+                begin_write = False
+                break
+            if begin_write:
+                self._write_fixed(line, filename)
 
-        f = ' '.join(f.split())
+#    def _cleaning(self, f, filename):
+#
+#        f = ' '.join(f.split())
         # first clean comments
-        comments_regex = re.compile(r'/\**\s\*?\s?.*?\s?\*/|//\s?.*?[;\n]',
-                                    re.I)
-        f = comments_regex.sub('', f)
-        if self.linestart.search(f) is None:
-            logging = open('LOGFILE.txt', 'a')
-            logging.write('linestart.search => None: ' + filename)
-            logging.close()
-        start_indx = self.linestart.search(f).end()
-        if self.lineend.search(f) is None:
-            logging = open('LOGFILE.txt', 'a')
-            logging.write('lineend.search => None: ' + filename)
-            logging.close()
-        end_indx = self.lineend.search(f).start()
-        student_code_start = f[start_indx:end_indx]
+        # if filename == '757092_assignsubmission_file_SelectQuery.java':
+        # comments_regex = re.compile(
+        #   r'/\**\s\*?\s?.*?\s?\*/|//*\s?.*?(?=})|(?=\n)', re.I)
+        # f = comments_regex.sub('', f)
+#        breakpoint()
+#        if self.linestart.search(f) is None:
+#            logging = open('LOGFILE.txt', 'a')
+#            logging.write('linestart.search => None: ' + filename + '\n')
+#            logging.close()
+#            return 0
+#        start_indx = self.linestart.search(f).end()
+#        if self.lineend.search(f) is None:
+#            logging = open('LOGFILE.txt', 'a')
+#            logging.write('lineend.search => None: ' + filename + '\n')
+#            logging.close()
+#            return 0
+#        end_indx = self.lineend.search(f).start()
 
-    def _analyze(self, f):
-        # comments
-        comments_regex = re.compile(r'/\**\s\*?\s?.*?\s?\*/|//\s?.*?[;\n]')
-        f = f.split()
-        f = ' '.join(f)
+#        return f[start_indx:end_indx]
 
-        breakpoint()
-        match = self.linestart.search(f)
-        assert match
-        start = match.start()
-        end = match.end()
-
-        final_code = f[start:end]
-        print(final_code)
+    def _write_fixed(self, text, filename):
+        fixed = open(self.pathtofixed + 'f' + filename, 'a')
+        fixed.write(text + '\n')
+        fixed.close()
 
     def format(self):
         self._read_space()
-        exit()
-
-        self._fixfilepath()
-        namelist = os.listdir(self.pathtoold)
-        self.linestart = ''.join(self.linestart.split())
-        self.lineend = ''.join(self.lineend.split())
-        for filename in namelist:
-            if os.path.isdir(filename) or filename[-5:] != ".java":
-                continue
-            linesnum = []
-            lineenum = 0
-            num = 0
-            check = False
-            with open(self.pathtoold + filename, 'r') as f:
-                for line in f:
-                    num += 1
-                    line = ''.join(line.split())
-                    if self.linestart in line:
-                        check = True
-                    elif check and not line.startswith(
-                            '/') and not line.startswith('*'):
-                        linesnum.append(num)
-                    elif line == self.lineend:
-                        lineenum = num
-            num = 0
-            check = False
-            with open(self.pathtofixed + 'f' + filename, 'w') as nf:
-                with open(self.pathtoold + filename, 'r') as f:
-                    for line in f:
-                        num += 1
-                        if num in linesnum:
-                            nf.write(line)
-                        if num + 1 == lineenum:
-                            break
 
 
 if __name__ == "__main__":
@@ -121,11 +117,14 @@ if __name__ == "__main__":
     #    pathtofixed = input("fixed file path: ")
     #    pathtoold = input("pathtoold to read from: ")
     #    format(linestart, lineend, pathtofixed, pathtoold)
-    linestart = re.compile(r'lookup\(string id\)\s?.*?{\s*',
-                           re.I)
+    linestart = re.compile(r'eval\s?\(table table\)\s?.*?{\s*', re.I)
 
-    lineend = re.compile(r'.(?=public list<(insttuple)|(table.insttuple)> lookupbydept)', re.I)
-    pathtofixed = 'javahw1/fixed'
-    pathtoold = 'javahw1/'
+    lineend = re.compile(r'.(?=@Override public\s?string\s?tostring\(\))',
+                         re.I)
+
+    # re.compile(
+    #   r'.(?=public list<(insttuple)|(table.insttuple)> lookupbydept)', re.I)
+    pathtofixed = 'javahw3/newfixed'
+    pathtoold = 'javahw3/'
     a = fmt(linestart, lineend, pathtofixed, pathtoold)
     a.format()
