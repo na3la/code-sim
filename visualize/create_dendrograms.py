b3a5058da3decb03bd549c20ac05c79c7abd4b92
na3la <tm.sc@protonmail.com>
2020-01-30 20:11:44 -0800

    added try/except for plt.show() in visualize/create_dendrogram

--- visualize/create_dendrograms.py
+++ visualize/create_dendrograms.py
@@ -1,9 +1,6 @@
-import pandas as pd
-import matplotlib
-import matplotlib.backends.backend_svg as svg
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist
-from scipy.cluster.hierarchy import dendrogram, linkage 
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 
 def plot(dat, fname, title):
@@ -11,5 +8,7 @@ def plot(dat, fname, title):
     plt.title("Distance Measure: " + title)
     plt.xticks(size=12)
     plt.savefig(fname + ".svg")
-    plt.show()
-
+    try:
+        plt.show()
+    except Exception as e:
+        print(e)
