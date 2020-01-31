bb519c72185e34ad72a12238ce8fdbb15795ffd3
na3la <tm.sc@protonmail.com>
2020-01-30 18:48:45 -0800

    removed backups

--- code-sim/backups/tokenizer_bak/driveropen.py
+++
@@ -1,7 +0,0 @@
-from file_feeder import _file_feeder
-from open_lex import open_lex
-
-for x in open_lex(
-        _file_feeder(
-            '/home/anon/notebooks/code-sim/tokenizer/javahw1/newfixed/')):
-    print(x)
